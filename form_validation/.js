8beeec06d0daa90827ca4a0d29bbfd72e9b76f03
HUDSON <163627779+Hudson-bot@users.noreply.github.com>
2025-05-28 10:26:00 +0530

    Create .js

---
+++ form_validation/.js
@@ -0,0 +1,8 @@
+<script>
+  function validate form()
+let user = document.getelementbyId("Usermame").value;
+let pass = document.getelementbyId("Password").value;
+if(user.length<5)
+  alert("
+  return 
+if(!/\
